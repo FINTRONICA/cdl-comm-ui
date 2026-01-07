140595462f94e9d20164412942bbec84f28a9b1d
rahulcdl <rahul.b@codedecodelabs.com>
2026-01-07 19:17:23 +0530

    fixed logo visible issue

--- src/components/organisms/Sidebar/Logo.tsx
+++ src/components/organisms/Sidebar/Logo.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import { getPublicAssetPath } from '@/utils/basePath'
 
 const Logo: React.FC = () => {
   return (
-    <div className="flex items-center w-full mb-2 mt-2">
+    <div className="flex items-center w-full mt-2 mb-2">
       <Image
-        src="/Logo.png"
+        src={getPublicAssetPath('/Logo.png')}
         alt="Escrow Central Logo"
         width={100}
         height={40}
